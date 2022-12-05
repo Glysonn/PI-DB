3be1e2e22eac31c610f1493904693e351fff3c65
Glysonn <kauaglyson@gmail.com>
2022-12-05 00:13:34 -0300

    -INCOMPLETO- SCRIPT: Criar Triggers

--- 3. Modelo Físico/Triggers.sql
+++ 3. Modelo Físico/Triggers.sql
@@ -1,7 +1,51 @@
+-- TRIGGER 1
+-- Preenche automaticamente a coluna NomeCompleto da tabela usuarios
 DELIMITER //
-CREATE TRIGGER NomeCompletoT BEFORE INSERT ON ABC
+CREATE TRIGGER tgr_usuarios_Insert_Bf BEFORE INSERT ON usuarios
 FOR EACH ROW
 BEGIN
 	SET NEW.NomeCompleto = CONCAT(NEW.Nome, ' ', NEW.Sobrenome);
+END //
+DELIMITER ;
+
+-- TRIGGER 2
+-- Salva o mês e ano da data de vencimento
+DELIMITER //
+CREATE TRIGGER tgr_cartaodecredito_Insert_Bf BEFORE INSERT ON cartaodecredito
+FOR EACH ROW
+BEGIN
+	SET NEW.MesAnoVencimento = DATE_FORMAT(NEW.DataVencimento, "%y/%m");
+END //
+DELIMITER ;
+
+-- TRIGGER 3
+-- Insere a hora atual no campo "Data" automaticamente
+DELIMITER //
+CREATE TRIGGER tgr_doacoes_Insert_Bf BEFORE INSERT ON doacoes
+FOR EACH ROW
+BEGIN
+	SET NEW.`Data` = NOW();
+END //
+DELIMITER ;
+
+
+-- TRIGGER 4
+-- Insere a hora atual no campo "DataPagamento" automaticamente
+DELIMITER //
+CREATE TRIGGER tgr_pagamentos_Insert_Bf BEFORE INSERT ON pagamentos
+FOR EACH ROW
+BEGIN
+	SET NEW.DataPagamento = NOW();
+END //
+DELIMITER ;
+
+
+-- TRIGGER 5
+-- Insere a Bandeira do cartão de acordo com o seu número
+DELIMITER //
+CREATE TRIGGER tgr_Numerocartaodecredito_Insert_Bf BEFORE INSERT ON cartaodecredito
+FOR EACH ROW
+BEGIN
+	SET NEW.Bandeira = BandeiraDoCartao(NEW.Numero);
 END //
 DELIMITER ;
