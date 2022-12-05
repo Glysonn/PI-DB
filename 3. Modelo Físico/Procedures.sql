b81c2f383ad3b1df2c6023048309e6792342b13e
Glysonn <kauaglyson@gmail.com>
2022-12-05 00:13:18 -0300

    -INCOMPLETO- SCRIPT: Criar SPs e SFs

--- 3. Modelo Físico/Procedures.sql
+++ 3. Modelo Físico/Procedures.sql
@@ -25,4 +25,21 @@ END //
 DELIMITER ;
 -- ---------------------------------------------------
 
-
+-- FUNCTION 1
+-- RETORNA A BANDEIRA DO CARTÃO DE CRÉDITO A PARTIR DE SEU NÚMERO
+DELIMITER //
+CREATE FUNCTION BandeiraDoCartao(NumeroCartao VARCHAR(19)) RETURNS VARCHAR(45) DETERMINISTIC
+BEGIN
+	DECLARE Bandeira VARCHAR(45);
+	SELECT CASE
+		WHEN substring(NumeroCartao FROM 1 FOR 2) LIKE "4%" THEN "VISA"
+		WHEN substring(NumeroCartao FROM 1 FOR 2) LIKE "51" OR substring(NumeroCartao FROM 1 FOR 2) LIKE "52" OR substring(NumeroCartao FROM 1 FOR 2) LIKE "53" OR substring(NumeroCartao FROM 1 FOR 2) LIKE "54" OR substring(NumeroCartao FROM 1 FOR 2) LIKE "55" THEN "Mastercard"
+		WHEN substring(NumeroCartao FROM 1 FOR 2) LIKE "36" OR substring(NumeroCartao FROM 1 FOR 2) LIKE "38" THEN "Diners Club"
+		WHEN substring(NumeroCartao FROM 1 FOR 4) LIKE "6011" OR substring(NumeroCartao FROM 1 FOR 2) LIKE "65" THEN "Discover"
+		WHEN substring(NumeroCartao FROM 1 FOR 2) LIKE "35" THEN "JBC"
+		WHEN substring(NumeroCartao FROM 1 FOR 2) LIKE "34" OR substring(NumeroCartao FROM 1 FOR 2) LIKE "37" THEN "American Express"
+		ELSE NULL
+	END INTO Bandeira;
+  RETURN Bandeira;
+END //
+DELIMITER ;
