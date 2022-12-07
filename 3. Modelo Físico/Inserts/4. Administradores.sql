faf928c1946ab583e5550d777557fe9a04e69885
Glysonn <kauaglyson@gmail.com>
2022-12-07 18:00:26 -0300

    SCRIPT: Insert de dados

---
+++ 3. Modelo Físico/Inserts/4. Administradores.sql
@@ -0,0 +1,2 @@
+INSERT INTO administradores (`Usuario_CPF`)
+VALUES ('429.877.404-09'), ('003.401.585-06');
---
+++ 3. Modelo Físico/Inserts/7. Pagamentos.sql
@@ -0,0 +1,12 @@
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`)
+VALUES (NULL,30,NULL,'Boleto',NULL),
+(NULL,15,NULL,'Cartao',1),
+(NULL,150,NULL,'Cartao',2),
+(NULL,1200,NULL,'Cartao',3),
+(NULL,50,NULL,'Cartao',1),
+(NULL,20,NULL,'Pix',NULL),
+(NULL,15,NULL,'Pix',NULL),
+(NULL,53,NULL,'Pix',NULL),
+(NULL,12,NULL,'Pix',NULL),
+(NULL,180,NULL,'Pix',NULL),
+(NULL,120,NULL,'Pix',NULL);
