7753595790bbbb48e23382c7ae4ba589fa96dc2a
Glysonn <kauaglyson@gmail.com>
2022-12-13 17:54:17 -0300

    SCRIPT: Inserts

---
+++ 3. Modelo Físico/5. Inserts.sql
@@ -0,0 +1,122 @@
+-- INSERTS TABELA USUARIOS
+INSERT INTO usuarios (`CPF`,`Nome`,`Sobrenome`,`Email`,`Telefone`,`Senha`,`NomeCompleto`)
+VALUES ('003.401.585-06','FERNANDA','CASTRO MONTEIRO','fcastromonte@yahoo.com.br','(11) 98203-8087','$pbkdf2-sha256$50000$kjImBICQkjLGeK91bu1daw$oSiiMnMzDYWGX5wNhBX9sw1Cr1lZ6FAqXpkQiQbwzes',NULL),
+('812.135.137-82','JOAO','GOMES REIS','JgomesR@gmail.com','(11) 97273-8374','$pbkdf2-sha256$50000$R2gtBeBci/G.NyakNIZwjg$q82.IZ9.al0V1r4qUZSie5bGTX1.8TxZx/Y7iNNjO0U',NULL),
+('021.131.621-05','MARIA CLARA','NASCIMENTO CARDOSO','mcncardoso1202@hotmail.com','(11) 90117-7716','$pbkdf2-sha256$50000$C4FQ6p0TAgBAaA2htPZe6w$I53IYAbhxb.TFTni4vSUfgij8upD6dvn0VFkCzWWK/o',NULL),
+('212.341.773-23','MARIA','BATISTA SOUZA','MariaMBSMaria@outlook.com','(81) 92372-0362','$pbkdf2-sha256$50000$4VyrlfJ.D.F8711rDWHMuQ$P/ffL8iVXruY/alEcjpy3fRS1v4uTZ5bOOp8BmQjldI',NULL),
+('721.141.514-76','LARISSA CAROLINA','LOPES MENDES','Laralopesouza@gmail.com','(81) 95402-2692','$pbkdf2-sha256$50000$PGds7V3LOedcq3XOudf6/w',NULL),
+('986.836.624-03','JOAO PEDRO','GARCIA ALVES','Pedrinhojpkun@gmail.com','(87) 92629-2822','$pbkdf2-sha256$50000$5VzrPefcGwOAkJLSOgfAeA$j0rsQGG2.nuVAigR96NU51M13QQnoovgWEnzReLgVbI',NULL),
+('742.931.154-71','DAVI LUCAS','RODRIGUES','RodriguesLucasDavi@uol.com.br','(87) 92712-2428','$pbkdf2-sha256$50000$UKoVAsCYE2JMCeE8p9SaMw$Dnd8Vd/0e5QtlRkkx/CyX6Lagg80XpZ7wRIT4ZcTTQE',NULL),
+('123.456.789-00','GLYSON KAUÃ','O. S. CAVALCANTI','glysonkosilva@fac.pe.senac.br','(81) 97692-3337','$pbkdf2-sha256$50000$LCUEAEDIudfae0.J0XovRQ$RWlOGw/Et4QV9UQpDh7F1hCwNTIqlQvQ9q2tKZW5jLM',NULL),
+('845.813.694-52','ANA','MONTEIRO MARTINS','aninha_monte@gmail.com','(81) 99403-9323','$pbkdf2-sha256$50000$ea/V.r.39t6bU8o5B2CM8Q$4D39FKycwBYhLeWko2DE7IBrtRlaxuH2JlA/E./cuKg',NULL),
+('463.035.744-08','ANA CLARA','RIBEIRO LIMA SOUZA','claranasz@hotmail.com','(81) 99665-4153','$pbkdf2-sha256$50000$././FyIEAABgrBXCeC9FaA$SErKyRDaaeixAg2m3xD37aveyyYZUD1vjctrt95Zass',NULL),
+('331.761.824-81','ALINE','FERREIRA GONÇALVES','AlineRastafari@gmail.com','(87) 97212-5887','$pbkdf2-sha256$50000$2PufU.r9//./dw5BaO19jw$T0b7Yfe8af1K2SEQf1DE0osksxrIXKfWwafL2xL1nwk',NULL),
+('295.705.164-88','NEYMAR','DA SILVA SANTOS JUNIOR','jogayjoga@outlook.com','(87) 97538-3462','$pbkdf2-sha256$50000$CoEQwjhHyBnjnLN2Tolxrg$Tl557/kWtJZL.WQLIsitnq36YuLeklpNJ4uzbDdWVNM',NULL),
+('522.987.264-00','LUIZA','SERAFIM','luizaa0712@gmail.com','(87) 99129-6738','$pbkdf2-sha256$50000$C.Hce8/ZG2MMAeB8jzHGWA$KpmXrBQeMPwpBJus3ZjG5M1eOuH1s8fzYVhWhKjMgFg',NULL),
+('514.698.984-20','HELOÍSA HELENA','MARQUES FERRAZ','helohele5@outlook.com','(87) 98303-2858','$pbkdf2-sha256$50000$WyvFeI.xdo6x9n4vBUBIyQ$JVkrMUswSNFeCOicdhpxexSTwkEaBAzsReb0fJkeYGY',NULL),
+('376.365.854-80','CARLOS','ROCHA FERNANDES','amojesus123@gmail.com','(81) 97986-1467','$pbkdf2-sha256$50000$mRPifM85R6hVCuEcg1AK4Q$E.seT7zNc8Q895clljxCjn8IacvRudGJXm6NNRU4Ix4',NULL),
+('411.800.454-24','MANOEL','GOMES CANETA AZUL','manoelgomescantor@gmail.com','(81) 99868-2225','$pbkdf2-sha256$50000$H4NwTmlNKQVAaO1dK2VsbQ$0JPS4wn6VahOd4GICd1k9Dv2H9vhkzuYqEWSRzimdPI',NULL),
+('697.562.704-54','FRANCISCO ANTONIO','NUNES LOPES','franuneslopesnunes@hotmail.com','(81) 96927-8382','$pbkdf2-sha256$50000$T4lRyvmfc66V8v5/T4mREg$UrUBIBbdrU4I.DUgX06bcrUrdkt.fDN3tXx5aFr4Us0',NULL),
+('011.146.412-00','JULIANA','MORAES CASTRO','tosemcriatividade@gmail.com','(81) 99416-5695','$pbkdf2-sha256$50000$vvceA6AU4jwnJKS09n7P2Q$bY2wHb66HjukNZiBE0MmQ1sB3NbKkNrnIqvfvFDo8NI',NULL),
+('429.877.404-09','ADRIANA','DE ALMEIDA','drinaalmeida@gmail.com','(81) 98631-6288','$pbkdf2-sha256$50000$eQ.BsDbmfC9FCEHI.d9biw$.D89HEaClDuVTdn6OKXGA9CWrmXxukTSqItjGUOqPZI',NULL),
+('858.296.124-37','ANA','SOARES MOREIRA','moreirana@hotmail.com','(87) 98869-6636','$pbkdf2-sha256$50000$MYawFmJs7V3L.b/XmnMu5Q$7/0JoiPe4e/rm34NTI1Up38yLPkVslhjnanRNAll8kA',NULL),
+('309.608.974-45','KELLY','HIRATA','hiratakelly@gmail.com','(81) 98923-3331','$pbkdf2-sha256$50000$LkUIwThn7L1XSskZ4xxjDA$IoLIMJEnTMbKkSwPYH2fWiqYoOmbi10tsWP7t2Qvk4k',NULL),
+('279.979.414-95','ROHAN','KISHIBE','kishibero@gmail.com','(81) 98424-3154','$pbkdf2-sha256$50000$H.NcS.n933vPWUtpLaW0dg$Hkkd/4.7F8AotCIFNW/mme7MqGt30VglbHzPA1wSwmQ',NULL),
+('870.879.004-39','PAULA','DUARTE GOMES','duartepaula@gmail.com','(87) 98872-8627','$pbkdf2-sha256$50000$znkPQUhpTQlBKGXMeW/tnQ$IYSRsCzFJToQATXM80Q4dguhE1GfpJoJ1xbxQkXqS6Y',NULL),
+('347.646.144-05','CARLOS ANTONIO','FREITAS','freitascarlos@gmail.com','(22) 99134-1815','$pbkdf2-sha256$50000$jPHe29u7N6aUUqo1RiiFkA$WeZR8S6ICJmaBgh3F4qgVD9Nyc/nsRFib07K4j0aEc4',NULL),
+('361.119.764-51','BRUNO','NASCIMENTO SILVA','bruninasc@gmail.com','(24) 98152-0474','$pbkdf2-sha256$50000$CyFEqLWWcs45hxCCUErp/Q$u40J9c9EJVrBUxZ/owSQXJ2SDKD3RFfQY8eu.FSjwzI',NULL),
+('198.118.514-33','FLAVIA ANA','SANTOS AMARAL','Flaviana@gmail.com','(21) 98086-3265','$pbkdf2-sha256$50000$NqYU4tw7J8SYU.o9Z4zReg$KwdpOF5vHDjXK/6oCnf0MhMcpOO7EfRrAZ27sNakBaM',NULL);
+-- --------------------------------------------------------------------------------------------------------------
+
+-- INSERTS TABELA CARTÃO DE CRÉDITO
+INSERT INTO cartaodecredito (`Numero`,`CVV`,`DataVencimento`,`AnoMesVencimento`,`Bandeira`, `NomeTitular`)
+VALUES ('6011 2329 1152 1816',050,'2030-02-00',NULL, NULL, "HELOÍSA HELENA M FERRAZ"),
+('3463 6883 6202 8441', 155, '2027-06-00', NULL, NULL, "GLYSON KAUA O S CAVALCANTI"),
+('4389 3514 7562 8012',129,'2027-08-00',NULL, NULL, "MANOEL G C AZUL"),
+('3478 7047 3232 8100', 207, '2030-11-00', NULL, NULL, "KELLY HIRATA"),
+('5346 2915 7626 3377',191,'2026-02-00',NULL, NULL, "JOAO PEDRO G ALVES"),
+('5576 3284 5336 4547',651,'2025-06-00',NULL, NULL, "CARLOS R FERNANDES"),
+('5176 2881 7872 0130',491,'2029-08-00',NULL, NULL, "MARIA B SOUZA"),
+('5417 6687 5181 4070',741,'2028-01-00',NULL, NULL, "MARIA CLARA N CARDOSO"),
+('5273 3910 2249 6584',159,'2023-06-00',NULL, NULL, "FLAVIA ANA S AMARAL"),
+('5341 7542 9637 5720', 660, '2027-01-00',NULL, NULL, "BRUNO N SILVA");
+-- --------------------------------------------------------------------------------------------------------------
+
+-- INSERTS TABELA USUÁRIOS_CARTAODECREDITO
+INSERT INTO usuarios_cartaodecredito (`Usuarios_CPF`,`CartaoDeCredito_Numero`)
+VALUES ('123.456.789-00','4389 3514 7562 8012'),
+('411.800.454-24','4389 3514 7562 8012'),
+('123.456.789-00','3463 6883 6202 8441'),
+('295.705.164-88','3463 6883 6202 8441'),
+('309.608.974-45','3478 7047 3232 8100'),
+('198.118.514-33','5273 3910 2249 6584'),
+('212.341.773-23','5176 2881 7872 0130'),
+('361.119.764-51','5341 7542 9637 5720'),
+('986.836.624-03','5346 2915 7626 3377'),
+('021.131.621-05','5417 6687 5181 4070'),
+('376.365.854-80','5576 3284 5336 4547'),
+('514.698.984-20','6011 2329 1152 1816');
+-- --------------------------------------------------------------------------------------------------------------
+
+-- INSERTS TABELA ADMINISTRADORES
+INSERT INTO administradores (`Usuario_CPF`)
+VALUES ('429.877.404-09'), ('003.401.585-06');
+-- --------------------------------------------------------------------------------------------------------------
+
+-- INSERTS TABELA PUBLICACOES (AS IMAGENS SERIAM INSERIDAS NA APRESENTAÇÃO, JÁ QUE ELA TEM QUE ESTAR NO CAMINHO ESPECÍFICO DO COMPUTADOR)
+INSERT INTO publicacoes (`ID`,`Administradores_Usuario_CPF`,`Data`,`Legenda`,`Imagem`) VALUES
+(NULL,'003.401.585-06',NULL,'A próxima doação de sopa ocorrerá nesta quarta-feira(14/12)...',NULL),
+(NULL,'003.401.585-06',NULL,'Saiba de tudo que ocorreu na nossa última reunião...',NULL),
+(NULL,'429.877.404-09',NULL,'Nossa reunião para ajudar moradores de rua ocorr...',NULL),
+(NULL,'429.877.404-09',NULL,'Legenda aqui.....',NULL),
+(NULL,'003.401.585-06',NULL,'Legenda aqui....',NULL);
+-- --------------------------------------------------------------------------------------------------------------
+
+-- INSERTS TABELA DOAÇÕES E TABELA PAGAMENTOS (DEVEM SER PREENCHIDAS AO MESMO TEMPO, OU SEJA, UM ATRÁS DO OUTRO)
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'123.456.789-00',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),30,NULL,'Boleto',NULL);
+-- 
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'198.118.514-33',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES(last_insert_id(),15,NULL,'Cartao',1);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'309.608.974-45',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),150,NULL,'Cartao',2);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'411.800.454-24',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),1200,NULL,'Cartao',3);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'411.800.454-24',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),150,NULL,'Pix', NULL);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'411.800.454-24',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),552,NULL,'Boleto', NULL);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'198.118.514-33',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),50,NULL,'Cartao',1);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'003.401.585-06',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),20,NULL,'Pix',NULL);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'011.146.412-00',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),15,NULL,'Pix',NULL);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'021.131.621-05',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),53,NULL,'Pix',NULL);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'279.979.414-95',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),12,NULL,'Pix',NULL);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'295.705.164-88',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),180,NULL,'Pix',NULL);
+--
+INSERT INTO doacoes (`ID`,`Usuarios_CPF`,`Data`) VALUES (NULL,'331.761.824-81',NULL);
+INSERT INTO pagamentos (`Doacoes_ID`,`Valor`,`DataPagamento`,`Tipo`,`Parcelas`) VALUES (last_insert_id(),120,NULL,'Pix',NULL);
+-- --------------------------------------------------------------------------------------------------------------
+
+
+
+
+-- Dados que serão inseridos para serem DELETADOS
+INSERT INTO usuarios (`CPF`,`Nome`,`Sobrenome`,`Email`,`Telefone`,`Senha`,`NomeCompleto`) VALUES ("929.661.524-00", "Vou ser", "Apagado", "naotenhoemail@email.com", "(81)988888888", "$pbkdf2-sha256$50000$4fyf8x5jTAkhpLQ2ZizlnA$wOHwL.Vyc4tFTDx.X3zWFd0.vU/MJacBBz5IiFAj1Zs", NULL), ("129.651.524-00", "Ueblers", "Xeblers", "ueblers@email.com", "(81)912895688", "$pbkdf2-sha256$50000$c46R8r4XAiCkFAKA8J7zPg$Oc43vGhJabZZdPUhtR2Z7koV5t8SMwVPBds5al.Wn9g", NULL);
+INSERT INTO administradores (`Usuario_CPF`) VALUES ('129.651.524-00');
+INSERT INTO publicacoes (`ID`,`Administradores_Usuario_CPF`,`Data`,`Legenda`,`Imagem`) VALUES (NULL,'129.651.524-00',NULL,'É pra ser apagado',NULL);
--- 3. Modelo Físico/Inserts/4. Administradores.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO administradores (`Usuario_CPF`)
-VALUES ('429.877.404-09'), ('003.401.585-06');
